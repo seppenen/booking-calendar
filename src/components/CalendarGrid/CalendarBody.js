08c3e31d7de9a8d2cf39a5b5bbeaade6c1d6a009
Aleksandr Seppenen <alex.seppenen@gmail.com>
2021-10-16 12:37:51 +0200

    Created sidebar with time Added week number

--- src/components/CalendarGrid/CalendarBody.js
+++ src/components/CalendarGrid/CalendarBody.js
@@ -5,28 +5,50 @@ import moment from "moment";
 
 const GridWrapper = styled.div`
   display: grid;
-  background-color: #dddd;
-  grid-template-columns: repeat(7,1fr);
+  background-color: #fff;
+  grid-template-columns: repeat(8,1fr);
   grid-gap: 1px;
   `;
 
 const TimeCellWrapper = styled.div`
-  height: 30px;
-  background-color: #fff;
+  height: 20px;
+  padding: 10px;
+  background-color: #dddd;
   display: flex;
   justify-content: center;
+ 
   `;
 
 const DayWrapper = styled.div`
-  margin: 15px;
+  height: 10px;
+  padding: 10px;
   display: flex;
   justify-content: center;
+  
+`
+const SideWrapper = styled.div`
+
+  background-color: #fff;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 1px;
+
+`
+
+const TimeSheetWrapper = styled.div`
+  background-color: #fff;
+  margin-top: 1px;
+  display: grid;
+  grid-template-columns: repeat(1,1fr);
+  grid-gap: 1px;
+
+  
 `
 
 
 const CalendarBody = ({startDay}) => {
 
-    const startHour = moment().startOf('week').hour('9').minute('00').subtract(1,'days')
+    const startHour = moment().startOf('week').hour('9').minute('00').subtract(1,'days').subtract(30,'minutes')
     const [selectedDay, setSelectedDay] = useState(moment().unix())
 
     const timeArray = [...Array(24)].map(()=> startHour.add(30, 'minutes').clone())
@@ -38,22 +60,39 @@ const CalendarBody = ({startDay}) => {
     return(
         <>
         <GridWrapper>
-            {
+
+            <SideWrapper>
+                <DayWrapper>
+                    Week {moment().week()}
+
+                </DayWrapper>
+                {
+                    timeArray.map((timeItem  ) => (
+                        <TimeCellWrapper key={timeItem.unix()}>
+                                {timeItem.format('HH:mm')}
+                        </TimeCellWrapper>
+                    ))
+                }
+                    </SideWrapper>
+                    {
                 days.map((dayItem ) => (
                     <div key={dayItem.format('dddd')}>
                         <DayWrapper>
                        {dayItem.format('dddd')}
                         </DayWrapper>
+                        <TimeSheetWrapper>
                         {
                             timeArray.map((timeItem  ) => (
-                                <TimeCellWrapper key={timeItem.format()}>
+                                <TimeCellWrapper key={timeItem.unix()}>
                                     {increaseDay(timeItem)}
                                   <button value={timeItem.unix()} onClick={(e)=>selectDay(e)}>
-                                   {timeItem.format('HH:mm')}
+                                      Available  {/*{timeItem.format("LLL")}*/}
                                   </button>
+
                                 </TimeCellWrapper>
                             ))
                         }
+                        </TimeSheetWrapper>
                     </div>
                 ))}
         </GridWrapper>
